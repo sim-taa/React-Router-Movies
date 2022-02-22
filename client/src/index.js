ab29384e543ff2e7cb47f8e6f2fed8e3e79ffd0b
Simone Taarnskov <simone.taarnskov@gmail.com>
2022-02-21 16:26:49 -0800

    Wrapped app with router

--- client/src/index.js
+++ client/src/index.js
@@ -6,3 +6,13 @@ import App from './App';
 
 // You'll need to wrap <App /> for routing to work
 ReactDOM.render(<App />, document.getElementById('root'));
+
+render(
+    // Wrap the <App /> in a provider
+    <Router>
+      <App />
+    </Router>
+    , document.querySelector('#root')
+  )
+
+  
